97d30d3246f573af2ccd582e780fd807bf722c81
KolknevaAngelina <115538324+KolknevaAngelina@users.noreply.github.com>
2024-01-16 23:19:46 +0300

    Работа с Retrofit2, OkHttpClient, HttpLoggingInterceptor V2.0 Добавлена возможность отслеживать что отправляет и получает Retrofit2 по Http протоколу

---
+++ app/src/main/java/com/example/retrofitproject/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.retrofitproject
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.retrofitproject.retrofit.ProductApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.tv)
+        val b = findViewById<Button>(R.id.button)
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://dummyjson.com").client(client)
+            .addConverterFactory(GsonConverterFactory.create()).build()
+        val productApi = retrofit.create(ProductApi::class.java)
+
+        b.setOnClickListener {
+            CoroutineScope(Dispatchers.IO).launch {
+                val product = productApi.getProductById(3)
+                runOnUiThread {
+                    tv.text = product.title
+                }
+            }
+        }
+    }
+}
